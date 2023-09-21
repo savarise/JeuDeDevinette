f7489233b10b6492e536b48e2927e5c510a1a388
savarise <143745809+savarise@users.noreply.github.com>
2023-09-21 11:45:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
-def compter_mots(chaine):
-    # split() permet de separer une phrase en chaines de characteres en les separant par un espace (whitespace)
-    # on utilise donc le chaine (ce que on met sur la fonction) et on le split()
-    mots = chaine.split()
+import random #on doit import random pour pouvoir declarer ses fonctions
+x = random.randint(0, 100)
+essai = -1 #il faut se souvenir de ne pas mettre essai a 0 quand on le declare sinon si lordinateur choisi 0 comme x, on va instatanement gagner
+nb_essai = 0
 
-    # on utlise la fonction len() pour compter le nombre de chaines de characteres qu'on a dans le chaine.split()
-    nombre_de_mots = len(mots)
+def QuiterOuRejouer(): #cette fonction est pour lorsque lessai est le bon nombre
+    global nb_essai #ceci fait que quand on declare la variable nb_essai, ca modifie cette variable hors de la fonction
+    print("Bravo! Bonne reponse \n Vous avez reussi en: " + str(nb_essai) + " essai(s)")
+    quit = str(input("Voulez-vous faire une autre partie (o/n)?"))
+    if quit == "n":
 
-    print("Le nombre de mots dans la chaine est :", nombre_de_mots)
-# alors, on utilise la fonction sur ce que l'utilisateur met comme input
-chaine_de_caracteres = input("Met des chaines de characteres et je vais compter combien il y en a.")
-compter_mots(chaine_de_caracteres)
+        print("Merci et au revoir...")
+        exit()
+    else:
+        nb_essai = 0
+        global x
+        x = random.randint(0, 100)
+        questioner() #donc si on ne quitte pas, ca recommence le procesus du jeu
+
+def essayer(): #cette fonction est pour chaque mauvais essai
+    essai = int(input())
+
+    global nb_essai
+    nb_essai += 1 # puisque ++ ne fonctionne pas en python, on doit utiliser +=
+    if essai < x:
+        print("x>essai")
+    elif essai > x:
+        print("x<essai")
+    else:
+        QuiterOuRejouer()
+
+def questioner(): #cette fonction demarre le jeu
+    print(str(x))
+    print("J'ai choisi un nombre aléatoire entre 0 et 100, essaye de le deviner")
+    while x != essai: #ceci va laisser lutilisateur reesayer de deviner chaque essai
+        essayer()
+questioner()
